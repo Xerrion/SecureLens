05f24ecbc1f0271239642f7f01e51f0024a90603
Jeppe Hartmann Jacobsen <jeppeh92@gmail.com>
2024-12-08 00:26:56 +0100

    Create AdminByRequestClient.cs

---
+++ SecureLens/AdminByRequestClient.cs
@@ -0,0 +1,27 @@
+﻿namespace DefaultNamespace;
+
+public class AdminByRequestClient
+{
+    private string BaseUrl { get; }
+    private string Startdate 
+    private string Enddate
+    private string Status
+    
+    public AdminByRequestClient(string baseUrl, string apiKey)
+    {
+        BaseUrl = baseUrl;
+        ApiKey = apiKey;
+    }
+
+    public List<string> fetchInventoryData()
+    {
+        var client = new RestClient(BaseUrl);
+        var request = new RestRequest("inventory", Method.GET);
+        request.AddHeader("Authorization", "Bearer " + ApiKey);
+        var response = client.Execute(request);
+        var content = response.Content;
+        var inventory = JsonConvert.DeserializeObject<List<string>>(content);
+        return inventory;
+    }
+    
+}
